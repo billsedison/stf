a4c324510b1b5afe36670c8e778230b2a4c2a71b
Jiangliwu <jiangliwu.ipple@gmail.com>
2019-06-06 10:28:03 +0800

    add button function and screenshot

--- res/app/control-panes/screenshots/screenshots-controller.js
+++ res/app/control-panes/screenshots/screenshots-controller.js
@@ -6,12 +6,33 @@ module.exports = function ScreenshotsCtrl($scope) {
     $scope.screenshots = []
   }
 
+
   $scope.shotSizeParameter = function(maxSize, multiplier) {
     var finalSize = $scope.screenShotSize * multiplier
     var finalMaxSize = maxSize * multiplier
 
     return (finalSize === finalMaxSize) ? '' :
-    '?crop=' + finalSize + 'x'
+      '?crop=' + finalSize + 'x'
+  }
+
+  $scope.getIOSImageWidth = function(maxSize, multiplier) {
+    var finalSize = $scope.screenShotSize * multiplier
+    return finalSize
+  }
+
+  /**
+   * open base64 data url
+   * @param base64URL the base 64 url
+   */
+  function debugBase64(base64URL) {
+    const win = window.open()
+    win.document.write('<title>screen shot image preview</title><iframe src="' + base64URL + '" ' +
+      'frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; ' +
+      'height:100%;" allowfullscreen></iframe>')
+  }
+
+  $scope.open = function(dataUrl) {
+    debugBase64(dataUrl)
   }
 
   $scope.takeScreenShot = function() {
